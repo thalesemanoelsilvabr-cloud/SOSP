082fc9d0ec330f764d2dbf271682fc2bb4a5dfd1
Thales <thalesemanoelsilva.br@gmail.com>
2026-03-15 00:26:30 -0300

    Add AppLoader class for SOSP application loading

---
+++ Apploader.java
@@ -0,0 +1,6 @@
+// AppLoader.java - Execução de programas nativos SOSP
+public class AppLoader {
+    public static void main(String[] args) {
+        System.out.println("SOSP: Carregando aplicação educacional...");
+    }
+}
